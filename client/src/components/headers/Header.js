c50cb63bd1fe847a2526ed21ee4ac357a792881f
Sagar Desai <desai.sagar2002@gmail.com>
2022-04-19 10:01:12 +0530

    Authentication Done.

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -14,7 +14,7 @@ function Header() {
     const [menu, setMenu] = useState(false)
 
     const logoutUser = async () => {
-        await axios.get('/user/logout')
+        await axios.get('/bazar/logout')
 
         localStorage.removeItem('firstLogin')
 
